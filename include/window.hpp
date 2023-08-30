ed9629294a00d519ba67c3b99623e290c58a8dfa
Pascal Klinkenbergh <none>
2023-08-30 23:39:25 +0200

    use image, texture and sprite the draw terrain

--- include/window.hpp
+++ include/window.hpp
@@ -13,15 +13,15 @@ public:
     Window(Model& model, uint pixelSize)
         : model(model),
           pixelSize(pixelSize),
-          win(sf::VideoMode(model.getWidth() * pixelSize, model.getHeight() * pixelSize), "Path", sf::Style::Titlebar | sf::Style::Close),
-          pixels(model.getHeight(), vector<sf::RectangleShape>(model.getWidth(), sf::RectangleShape(sf::Vector2f(pixelSize, pixelSize)))) {
-        // set position of pixels
-        for (int y = 0; y < model.getHeight(); ++y)
-            for (int x = 0; x < model.getWidth(); ++x)
-                pixels[y][x].setPosition(sf::Vector2f(x * pixelSize, y * pixelSize));
-
+          win(sf::VideoMode(model.getWidth() * pixelSize, model.getHeight() * pixelSize), "Path", sf::Style::Titlebar | sf::Style::Close) {
         // set fps
         win.setFramerateLimit(60);
+
+        // drawing setup
+        img.create(model.getWidth(), model.getHeight());
+        texture.create(model.getWidth(), model.getHeight());
+        sprite.setTexture(texture);
+        sprite.setScale(pixelSize, pixelSize);
     }
 
     bool isOpen() {
@@ -37,26 +37,31 @@ public:
     }
 
     void render() {
-        // set new color of pixels and draw
+        // update pixels on img with model
         for (int y = 0; y < model.getHeight(); ++y) {
             for (int x = 0; x < model.getWidth(); ++x) {
-                // get height of point
+                // get point at (x, y)
                 Model::Point* point = model.getPoint(x, y);
-                uchar colVal = (uchar)(point->height * 255);
-                sf::Color col(colVal, colVal, colVal);
 
                 // set different color for start and end
                 if (point == model.getStart())
-                    col = sf::Color::Green;
+                    img.setPixel(x, y, sf::Color::Green);
                 else if (point == model.getEnd())
-                    col = sf::Color::Red;
-
-                // set color of pixel and draw
-                pixels[y][x].setFillColor(col);
-                win.draw(pixels[y][x]);
+                    img.setPixel(x, y, sf::Color::Red);
+                else {
+                    uchar colVal = (uchar)(point->height * 255);
+                    img.setPixel(x, y, sf::Color(colVal, colVal, colVal));
+                }
             }
         }
 
+        // update texture
+        texture.update(img);
+
+        // draw sprite that has that texture
+        win.draw(sprite);
+
+        // final drawing to screen
         win.display();
     }
 
@@ -68,5 +73,8 @@ private:
     Model& model;
     uint pixelSize;
     sf::RenderWindow win;
-    vector<vector<sf::RectangleShape>> pixels;
+
+    sf::Image img;
+    sf::Texture texture;
+    sf::Sprite sprite;
 };
